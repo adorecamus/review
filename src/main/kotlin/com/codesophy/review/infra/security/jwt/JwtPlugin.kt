62a84cf47e91f939a9ee4b4bdcc02e37e220d836
umin <adorecamus@gmail.com>
2024-01-15 03:05:07 +0900

    Feat: 토큰 검증 시 로그아웃 여부 확인

--- src/main/kotlin/com/codesophy/review/infra/security/jwt/JwtPlugin.kt
+++ src/main/kotlin/com/codesophy/review/infra/security/jwt/JwtPlugin.kt
@@ -1,10 +1,12 @@
 package com.codesophy.review.infra.security.jwt
 
+import com.codesophy.review.domain.exception.InvalidCredentialException
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jws
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Component
 import java.nio.charset.StandardCharsets
 import java.time.Duration
@@ -16,9 +18,14 @@ class JwtPlugin(
         @Value("\${auth.jwt.issuer}") private val issuer: String,
         @Value("\${auth.jwt.secret}") private val secret: String,
         @Value("\${auth.jwt.accessTokenExpirationHour}") private val accessTokenExpirationHour: Long,
+        private val redisTemplate: RedisTemplate<String, String>
 ) {
 
     fun validateToken(jwt: String): Result<Jws<Claims>> {
+        if (redisTemplate.opsForValue().get(jwt) != null) {
+            throw InvalidCredentialException()
+        }
+
         return kotlin.runCatching {
             val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
             Jwts.parser().verifyWith(key).build().parseSignedClaims(jwt)
